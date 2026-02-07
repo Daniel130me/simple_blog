895137d621d6073954de6ba3e98d9f66fd093d37
Daniel130me <kosokodaniel@gmail.com>
2026-02-08 00:42:24 +0100

    updated unused paths

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,16 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Home from './pages/Home';
-import PostDetailPage from './pages/PostDetailPage';
-import AdminLayout from './admin/pages/AdminLayout';
-import Dashboard from './admin/pages/Dashboard';
-import ManagePosts from './admin/pages/ManagePosts';
-import AddPostPage from './admin/pages/AddPostPage';
-import EditPostPage from './admin/pages/EditPostPage';
-import Ai from './pages/Ai';
-import Docs from './pages/Docs';
-import SwaggerDocs from './pages/swagger';
-
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import PostDetailPage from "./pages/PostDetailPage";
+import AdminLayout from "./admin/pages/AdminLayout";
+import Dashboard from "./admin/pages/Dashboard";
+import ManagePosts from "./admin/pages/ManagePosts";
+import AddPostPage from "./admin/pages/AddPostPage";
+import EditPostPage from "./admin/pages/EditPostPage";
+// import Ai from './pages/Ai';
+// import Docs from './pages/Docs';
+// import SwaggerDocs from './pages/swagger';
 
 function App() {
   return (
@@ -21,9 +20,9 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/post/:id" element={<PostDetailPage />} />
-          <Route path="/ai" element={<Ai />} />
+          {/* <Route path="/ai" element={<Ai />} />
           <Route path="/docs" element={<Docs />} />
-          <Route path="/swagger" element={<SwaggerDocs />} />
+          <Route path="/swagger" element={<SwaggerDocs />} /> */}
           <Route path="/admin" element={<AdminLayout />}>
             <Route index element={<Dashboard />} />
             <Route path="dashboard" element={<Dashboard />} />
